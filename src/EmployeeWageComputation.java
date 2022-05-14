7dcb8b149ab478a56e3b8662e07ffccc2ae0d986
Ritwik <deshrutu@gmail.com>
2022-05-14 19:29:53 +0530

    UC3 - Part Time

--- src/EmployeeWageComputation.java
+++ src/EmployeeWageComputation.java
@@ -1,6 +1,7 @@
 public class EmployeeWageComputation {
-    double isAbsent = Math.floor(Math.random() * 10) % 2;
+    double isAbsent = Math.floor(Math.random() * 10) % 3;
     int FULL_TIME = 1;
+    int PART_TIME = 2;
     int WAGE_PER_HOUR = 20;
     int empHrs = 0;
 
@@ -15,7 +16,9 @@ public class EmployeeWageComputation {
     void isAbsent(){
         if(isAbsent == 1){
             System.out.println("Employee is Present.");
-        }else {
+        } else if (isAbsent == 2) {
+            System.out.println("Employee is Present Part-Time.");
+        } else {
             System.out.println("Employee is Absent.");
         }
     }
@@ -24,7 +27,10 @@ public class EmployeeWageComputation {
         if(isAbsent == 1){
             empHrs = 8;
             System.out.println("Employee Daily Wage is: "+ empHrs * WAGE_PER_HOUR);
-        }else {
+        } else if (isAbsent == 2) {
+            empHrs = 4;
+            System.out.println("Employee Daily Wage is: "+ empHrs * WAGE_PER_HOUR);
+        } else {
             System.out.println("Employee Daily Wage is: "+ empHrs * WAGE_PER_HOUR);
         }
     }
